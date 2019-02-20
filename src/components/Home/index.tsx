3e57b38e62d1bcd64a67f99af00934228f185157
Tlaster <tlaster@outlook.com>
2019-02-20 19:48:32 +0800

    Update logo

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <div className="Home-Container">
             <div className="Home-Main-Container ms-slideUpIn20">
-                <img className="Home-Main-Icon" src="https://avatars1.githubusercontent.com/u/47548333" />
+                <img className="Home-Main-Icon" src="/logo.png" />
                 <span className="Home-Main-Title ms-font-su">ShibaJS</span>
                 <span className="Home-Main-Desc">
                     <FormattedMessage id="home.main.desc" />

1d3cf56ed669a39bdd350587e7857cbf833a0c38
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-11 15:57:17 +0000

    Deploy ShibaJS/ShibaJS.github.io to github.com/ShibaJS/ShibaJS.github.io.git:master

---
+++ precache-manifest.5adb71b252c48dfae9378ef2efcdcd58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67541f8cb7ba3bd0f925ca6435984713",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4e64f33fae6832253ae",
+    "url": "/static/css/main.ae6802b0.chunk.css"
+  },
+  {
+    "revision": "77adac35f7dfc6991a30",
+    "url": "/static/js/2.2a86eef7.chunk.js"
+  },
+  {
+    "revision": "d4e64f33fae6832253ae",
+    "url": "/static/js/main.0f937285.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
